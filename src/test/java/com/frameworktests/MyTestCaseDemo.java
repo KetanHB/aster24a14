8249cda2dedbffe942d34d0a7e703c5c1d3862ba
KetanHB <ketan.bajoria@gmail.com>
2025-06-29 11:40:48 +0530

    this is my first commit

---
+++ src/test/java/com/frameworktests/MyTestCaseDemo.java
@@ -0,0 +1,11 @@
+package com.frameworktests;
+
+import org.testng.annotations.Test;
+
+public class MyTestCaseDemo {
+
+	@Test
+	public void MyTestCaseDemo1() {
+		System.out.println("Hello Run!!!");
+	}
+}
